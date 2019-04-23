f803a4918f3da52982544a2758127f66c90259bd
gwu358 <gwu358@gmail.com>
2019-04-23 16:43:39 -0400

    working on order history

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 /**
  * COMPONENT
  */
+function dateConvert(time) {
+  const date = new Date(time);
+  return ((date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear());
+}
+
 export const UserHome = props => {
-  const {email, orderHistory} = props
+  const { email, orderHistory } = props
+  console.log(orderHistory)
   return (
     <div>
       <h3>Welcome, {email}</h3>
       {
-        orderHistory && orderHistory.map(order => {
-          return (
-            <h3>Date: {order.updatedAt}</h3>
-            
+        orderHistory && orderHistory.map((order, index) => {
+          return (<div key={index}>
+            <h2>Order # {orderHistory.length - index}:</h2>
+            <h2>total price: $ {(order.total / 100).toFixed(2)}</h2>
+            <h3>Date: {dateConvert(order.updatedAt)}</h3>
+            {
+              order.books.map((book) => {
+                return <div key={book.id}>
+                  {/* <figure className="image is-128x128"> */}
+                    <img src={book.imgUrl} width={100} height={200}/>
+                  {/* </figure> */}
+                  <h2>{book.title}</h2>
+                </div>
+              })
+            }
+            <br />
+          </div>
           )
         })
       }
